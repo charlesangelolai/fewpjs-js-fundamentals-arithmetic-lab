20354b29b596d6b1bc60c9e3280cee639b561810
Charles Lai <charlesangeloplai@gmail.com>
2021-03-02 15:23:23 -0500

    commit tree cbb8d1a38df4ba379de2fdfd5912c3cee22458fd

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * num1) + 1;
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max(num2, num3, num4, 20);
